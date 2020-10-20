fb906264db9f832215717f9db9e0b01b410c10ca
Anjalin06 <anjalinmenon99@gmail.com>
2020-10-20 15:37:15 +0530

    my second commit

--- FirstNetCoreApp/Program.cs
+++ FirstNetCoreApp/Program.cs
@@ -2,16 +2,16 @@
 
 namespace FirstNetCoreApp
 {
-    public delegate int MathOpDelegate(int M, int N);
+    //public delegate int MathOpDelegate(int M, int N);
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(DateTime.Now.ToString());
-            MathOpDelegate delAdd = Add;
-            int result = delAdd(55,32);
-            Console.WriteLine("result of add:\t"+result.ToString());
+            // MathOpDelegate delAdd = Add;
+            // int result = delAdd(55,32);
+            // Console.WriteLine("result of add:\t"+result.ToString());
             
         }
     }
