b55a7e7842f79934e428444f37b80024b69c22b2
Anjalin06 <anjalinmenon99@gmail.com>
2020-10-20 15:35:16 +0530

    my first commit

---
+++ FirstNetCoreApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstNetCoreApp
+{
+    public delegate int MathOpDelegate(int M, int N);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToString());
+            MathOpDelegate delAdd = Add;
+            int result = delAdd(55,32);
+            Console.WriteLine("result of add:\t"+result.ToString());
+            
+        }
+    }
+}
